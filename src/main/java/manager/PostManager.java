4dce964e894695112340939b912dd7d3c3b7f4a9
komisuomi <only-for-serzh@yandex.ru>
2020-10-10 22:49:36 +0300

    Added PostManager Class

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,110 @@
+package manager;
+
+import domain.CommentsInfo;
+import domain.Post;
+
+public class PostManager {
+    private Post[] id;
+    public Post[] getId() {
+        return null;
+    }
+
+    private Post[] fromId;
+    public Post[] getFromId() {
+        return null;
+    }
+
+    private Post[] createdBy;
+    public Post[] getCreatedBy() {
+        return null;
+    }
+
+    private Post[] text;
+    public Post[] getText() {
+        return null;
+    }
+
+    private Post[] date;
+    public Post[] getDate() {
+        return null;
+    }
+
+    private Post[] replyOwnerId;
+    public Post[] getReplyOwnerId() {
+        return null;
+    }
+
+    private Post[] replyPostId;
+    public Post[] getReplyPostId() {
+        return null;
+    }
+
+    private Post[] friendsOnly;
+    public Post[] getFriendsOnly() {
+        return null;
+    }
+
+    private Post[] copyright;
+    public Post[] getCopyright() {
+        return null;
+    }
+
+    private Post[] postType;
+    public Post[] getPostType() {
+        return null;
+    }
+
+    private Post[] signerId;
+    public Post[] getSignerId() {
+        return null;
+    }
+
+    private Post[] canPin;
+    public Post[] getCanPin() {
+        return null;
+    }
+
+    private Post[] canDelete;
+    public Post[] getCanDelete() {
+        return null;
+    }
+
+    private Post[] canEdit;
+    public Post[] getCanEdit() {
+        return null;
+    }
+
+    private Post[] isPinned;
+    public Post[] getIsPinned() {
+        return null;
+    }
+
+    private Post[] markedAsAds;
+    public Post[] getMarkedAsAds() {
+        return null;
+    }
+
+    private Post[] favorite;
+    public Post[] getFavorite() {
+        return null;
+    }
+
+    private Post[] postponedId;
+    public Post[] getPostponedId() {
+        return null;
+    }
+
+    private Post[] subscribed;
+    public Post[] getSubscribed() {
+        return null;
+    }
+
+
+
+    public <ownerId, domain, query, ownersOnly, count, offset> Post[] search() {
+        return null;
+    }
+
+    public <ownerId, postId> void delete() {
+    }
+}
