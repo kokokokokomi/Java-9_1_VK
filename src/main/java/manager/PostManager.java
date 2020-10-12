7586e13ba311ff6fe5dc23003f8c5d81a0fdebcf
komisuomi <only-for-serzh@yandex.ru>
2020-10-12 14:45:01 +0300

    Deleted Redundant PostManager Methods

--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -1,103 +1,27 @@
 package manager;
 
-import domain.CommentsInfo;
 import domain.Post;
 
 public class PostManager {
-    private Post[] id;
-    public Post[] getId() {
-        return null;
-    }
-
-    private Post[] fromId;
-    public Post[] getFromId() {
-        return null;
-    }
-
-    private Post[] createdBy;
-    public Post[] getCreatedBy() {
-        return null;
-    }
-
-    private Post[] text;
-    public Post[] getText() {
-        return null;
-    }
-
-    private Post[] date;
-    public Post[] getDate() {
-        return null;
-    }
-
-    private Post[] replyOwnerId;
-    public Post[] getReplyOwnerId() {
-        return null;
-    }
-
-    private Post[] replyPostId;
-    public Post[] getReplyPostId() {
-        return null;
-    }
-
-    private Post[] friendsOnly;
-    public Post[] getFriendsOnly() {
-        return null;
-    }
-
-    private Post[] copyright;
-    public Post[] getCopyright() {
-        return null;
-    }
-
-    private Post[] postType;
-    public Post[] getPostType() {
-        return null;
-    }
-
-    private Post[] signerId;
-    public Post[] getSignerId() {
-        return null;
-    }
-
-    private Post[] canPin;
-    public Post[] getCanPin() {
-        return null;
-    }
-
-    private Post[] canDelete;
-    public Post[] getCanDelete() {
-        return null;
-    }
-
-    private Post[] canEdit;
-    public Post[] getCanEdit() {
-        return null;
-    }
-
-    private Post[] isPinned;
-    public Post[] getIsPinned() {
-        return null;
-    }
-
-    private Post[] markedAsAds;
-    public Post[] getMarkedAsAds() {
-        return null;
-    }
-
-    private Post[] favorite;
-    public Post[] getFavorite() {
-        return null;
-    }
-
-    private Post[] postponedId;
-    public Post[] getPostponedId() {
-        return null;
-    }
-
-    private Post[] subscribed;
-    public Post[] getSubscribed() {
-        return null;
-    }
+    private int id;
+    private int fromId;
+    private int createdBy;
+    private String text;
+    private int date;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private String copyright;
+    private String postType;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean favorite;
+    private int postponedId;
+    private boolean subscribed;
 
 
     public Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
